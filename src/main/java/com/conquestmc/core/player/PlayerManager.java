fa85affb2d1e313fb0c1f589320d7f5d21227893
Matthew Cameron <matthew.cameron1@ucalgary.ca>
2020-07-02 16:37:43 -0700

    null checks

--- src/main/java/com/conquestmc/core/player/PlayerManager.java
+++ src/main/java/com/conquestmc/core/player/PlayerManager.java
@@ -113,6 +113,8 @@ public class PlayerManager {
     public void removePermissions(Player player) {
         PermissionAttachment attachment = null;
         for (PermissionAttachmentInfo info : player.getEffectivePermissions()) {
+            if (info.getAttachment() == null || info.getAttachment().getPlugin() == null)
+                continue;
             if (info.getAttachment().getPlugin() instanceof CorePlugin) {
                 attachment = info.getAttachment();
             }
